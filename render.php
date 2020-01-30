4f6bed26df24fdc4b7927dd6dbdc20cc42c74e65
Baptiste Langlade <baptouuuu@gmail.com>
2020-01-30 19:42:39 +0100

    fix render helper

--- render.php
+++ render.php
@@ -4,14 +4,15 @@ require __DIR__.'/vendor/autoload.php';
 
 use function Innmind\FileWatch\bootstrap;
 use Innmind\OperatingSystem\Factory;
+use Innmind\TimeWarp\Halt\Usleep;
 use Innmind\Url\Path;
 use Innmind\Server\Control\Server\Command;
 
 $os = Factory::build();
-$watch = bootstrap($os);
+$watch = bootstrap($os->control()->processes(), new Usleep, $os->clock());
 
-$watch(new Path(__DIR__.'/graph.dot'))(function() use ($os): void {
-    $output = $os
+$watch(Path::of(__DIR__.'/graph.dot'))(function() use ($os): void {
+    $process = $os
         ->control()
         ->processes()
         ->execute(
@@ -19,9 +20,9 @@ $watch(new Path(__DIR__.'/graph.dot'))(function() use ($os): void {
                 ->withShortOption('Tsvg')
                 ->withShortOption('o', 'graph.svg')
                 ->withArgument('graph.dot')
-        )
-        ->wait()
-        ->output();
-    echo $output;
+        );
+    $process->wait();
+
+    echo $process->output()->toString();
     echo 'rendered'."\n";
 });
